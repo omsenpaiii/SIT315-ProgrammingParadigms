0f0c76cedb8263e2ab1f496e48dd5f96282a40ea
Om Tomar <123073338+omsenpaiii@users.noreply.github.com>
2024-01-30 19:36:06 +0530

    Create PlantWateringSystem.ino

---
+++ Module1/PlantWateringSystem.ino
@@ -0,0 +1,53 @@
+#include <LiquidCrystal.h>
+
+const int TMP36 = A0;
+const int motor = 13;
+const int LedRed = 12;
+const int LedGreen = 11;
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("Automated Plant");
+  lcd.setCursor(0,1);
+  lcd.print("Watering System!");
+  pinMode(motor, OUTPUT);
+  pinMode(LedRed, OUTPUT);
+  pinMode(LedGreen, OUTPUT);
+  delay(2000);
+  lcd.clear();
+  lcd.print("Temp= ");
+  lcd.setCursor(0,1);
+  lcd.print("WaterPump= ");
+  Serial.println("System Initialized");
+}
+
+void loop() {
+  int value = analogRead(TMP36);
+  float Temperature = value * 500.0 / 1023.0;
+  lcd.setCursor(6, 0);
+  lcd.print(Temperature);
+  lcd.setCursor(11, 1);
+
+  Serial.print("Temperature: ");
+  Serial.print(Temperature);
+  Serial.print(" C | Water Pump: ");
+
+  if (Temperature > 50) {
+    digitalWrite(motor, HIGH);
+    digitalWrite(LedRed, HIGH);
+    digitalWrite(LedGreen, LOW);
+    lcd.print("ON ");
+    Serial.println("ON");
+  } else {
+    digitalWrite(motor, LOW);
+    digitalWrite(LedRed, LOW);
+    digitalWrite(LedGreen, HIGH);
+    lcd.print("OFF");
+    Serial.println("OFF");
+  }
+
+  delay(1000);
+}
