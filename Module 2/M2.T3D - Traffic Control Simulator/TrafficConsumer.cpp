8251b4ce968296d1eed7f1808141d64cbbdae6c7
Om Tomar <123073338+omsenpaiii@users.noreply.github.com>
2024-03-19 11:03:26 +0530

    Update TrafficConsumer.cpp

--- Module 2/M2.T3D - Traffic Control Simulator/TrafficConsumer.cpp
+++ Module 2/M2.T3D - Traffic Control Simulator/TrafficConsumer.cpp
@@ -5,28 +5,28 @@
 using namespace std;
 
 #define NUM_LIGHTS 12
-#define MEASUREMENTS 13     //12 per hour plus held avg value
+#define MEASUREMENTS 13     // 12 per hour plus held avg value
 
+// Function to print traffic details
 void printDetails(int array[NUM_LIGHTS][MEASUREMENTS]){
     for(int i = 1; i <= NUM_LIGHTS ; i++){
-        cout << to_string(i) + ":\t";
+        cout << to_string(i) + ":\t"; // Output the light number
         for (int j = 0 ; j < sizeof(array[i])/4 ; j++){
-            cout << to_string(array[i-1][j]) + ";";
+            cout << to_string(array[i-1][j]) + ";"; // Output each measurement for the light
         }
         cout<<endl;
     }
 }
 
-void caluclateTrafficVolume(int array[NUM_LIGHTS][MEASUREMENTS]){
+// Function to calculate average traffic volume
+void calculateTrafficVolume(int array[NUM_LIGHTS][MEASUREMENTS]){
     cout<<"calculating...\t";
     for (int i = 0 ; i < NUM_LIGHTS ; i++){
-        array[i][0] = 0;
+        array[i][0] = 0; // Initialize the sum of traffic volumes for each light
         for (int j = 1 ; j < MEASUREMENTS ; j++){
-            array[i][0]+=array[i][j];
+            array[i][0]+=array[i][j]; // Calculate the total traffic volume for each light
         }
-        //cout<< to_string(i)+":\t" + to_string(array[i][0]) + " = ";
-        array[i][0] = array[i][0] / (MEASUREMENTS-1);                   //integer division for simplicity
-        //cout<< to_string(array[i][0]) <<endl;
+        array[i][0] = array[i][0] / (MEASUREMENTS-1); // Calculate the average traffic volume for each light (integer division for simplicity)
     }
     cout<<"Done"<<endl;
 }
@@ -34,41 +34,43 @@ void caluclateTrafficVolume(int array[NUM_LIGHTS][MEASUREMENTS]){
 int main(){
 
     int Array[NUM_LIGHTS][MEASUREMENTS];
-    int arraySize = sizeof(Array[1])/4;      //4bit
-    long rawTime;
+    int arraySize = sizeof(Array[1])/4;      // 4 bits
 
+    long rawTime;
     int lightNum;
     int trafficVolume;
 
-    fstream log("log.txt", ios::in);
+    fstream log("log.txt", ios::in); // Open log file for reading
     if(!log.is_open()){
-        cout<<"Error: Log file not found"<<endl;
+        cout<<"Error: Log file not found"<<endl; // Output error message if log file cannot be opened
     } else {
-        cout<<"Retriving initial data from log.txt...\t";
+        cout<<"Retrieving initial data from log.txt...\t";
+        // Initialize array with zeros
         for(int i = 1 ; i <= NUM_LIGHTS ; i++){
             for (int j = 1 ; j < arraySize ; j++){
                 Array[i-1][j]=0;
             }
         }
         
+        // Read data from log file
         log >> rawTime >> lightNum >> trafficVolume;
-        //cout<< "\n" << ctime(&rawTime) << " - " << to_string(lightNum) << " - " << to_string(trafficVolume) << endl;
 
+        // Populate the array with data from the log file
         for(int k = 0 ; k < 12 ; k++){
             for(int i = 0 ; i < NUM_LIGHTS ; i++){
                 log >> rawTime >> lightNum >> trafficVolume;
-                //cout<<to_string(rawTime)<<to_string(lightNum)<<to_string(trafficVolume)<<endl;
 
+                // Shift the measurements in the array to the right
                 for(int j = sizeof(Array[0]) / sizeof(Array[0][0]) - 1 ; j>0;j--){
                     Array[lightNum-1][j] = Array[lightNum-1][j-1];
                 }
-                Array[lightNum-1][1] = trafficVolume;
+                Array[lightNum-1][1] = trafficVolume; // Store the new traffic volume measurement
             }
         }
-        log.close();
+        log.close(); // Close the log file
         cout<<"Complete"<<endl;
 
     }
-    caluclateTrafficVolume(Array);
-    //printDetails(Array);
+    calculateTrafficVolume(Array); // Calculate average traffic volumes
+    //printDetails(Array); // Uncomment this line to print traffic details
 }
